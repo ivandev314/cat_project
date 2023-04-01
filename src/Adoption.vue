8f8f720ea9fb7574d8aef42c049b9edab6dfe1d2
Ivan <ivan.dev314@gmail.com>
2023-04-01 17:36:18 +0200

    Set main state

--- src/Adoption.vue
+++ src/Adoption.vue
@@ -2,13 +2,32 @@
 import List from "./components/List.vue";
 import SearchSortFilter from "./components/SearchSortFilter.vue";
 import Slider from "./components/Slider.vue";
+import cats from "./assets/data/cats.json";
+import { reactive, ref } from "vue";
+
+const items = reactive(cats);
+const itemsShown = ref(20);
+const searchTerm = ref("");
+const sortType = ref("name");
+const sortDirection = ref("asc");
+let modalActive = ref(false);
+let condirmationActive = ref(false);
+let filter = reactive({
+  isYoungerThanSixMonths: false,
+  isYoungerThanTwelveMonths: false,
+  isBlack: false,
+});
+let slider = reactive({
+  sliderActive: false,
+  sliderLeftToRight: true,
+});
 </script>
 
 <template>
   <div>
     <Slider />
     <SearchSortFilter />
-    <List />
+    <List :items="items.cats" />
   </div>
 </template>
 
