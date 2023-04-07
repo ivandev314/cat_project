6c79d934d8bedad780549628a2bc5d748802b49d
“Ivan <ivan.dev314@gmail.com>
2023-04-07 16:40:15 +0200

    Change base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://ivandev314.github.io/cat_project/",
+  base: "/cat_project/",
   plugins: [vue()],
   resolve: {
     alias: {
