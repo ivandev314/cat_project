3630c2514b0e5912248a2076fa398e63d4a3b622
“Ivan <ivan.dev314@gmail.com>
2023-04-10 12:24:32 +0200

    Change base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/cat_project/" : "/",
+  base: "./",
   plugins: [vue()],
   resolve: {
     alias: {
